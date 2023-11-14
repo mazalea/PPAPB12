dc9c9286b20c0ace71321451141b7f9cf85bc352
mazalea <azalea.nareswari@gmail.com>
2023-11-14 09:46:45 +0700

    initial

---
+++ app/src/main/java/com/example/ppapb12/database/NoteDao.kt
@@ -0,0 +1,16 @@
+package com.example.ppapb12.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(note: Note)
+    @Update
+    fun update(note: Note)
+    @Delete
+    fun delete(note: Note)
+    @get:Query("SELECT * from note_table ORDER BY id ASC")
+    val allNotes: LiveData<List<Note>>
+}
